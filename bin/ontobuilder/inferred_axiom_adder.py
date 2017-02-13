c48abbd2f8b360e365b18956571209780af59e44
stuckyb <stuckyb@flmnh.ufl.edu>
2017-02-13 16:19:55 -0500

    Removed imports that are no longer needed.

--- bin/ontobuilder/inferred_axiom_adder.py
+++ bin/ontobuilder/inferred_axiom_adder.py
@@ -4,31 +4,12 @@
 
 # Python imports.
 import ontobuilder
-from labelmap import LabelMap
-from obohelper import isOboID, oboIDToIRI
-from ontology_entities import _OntologyClass, _OntologyDataProperty
-from ontology_entities import _OntologyObjectProperty, _OntologyAnnotationProperty
-from reasoner_manager import ReasonerManager
-from rfc3987 import rfc3987
 
 # Java imports.
-from java.io import File, FileOutputStream
 from java.util import HashSet
 from java.lang import UnsupportedOperationException
-from org.semanticweb.owlapi.apibinding import OWLManager
-from org.semanticweb.owlapi.model import IRI, OWLOntologyID
-from org.semanticweb.owlapi.model import AddAxiom, AddImport, RemoveImport
-from org.semanticweb.owlapi.model import SetOntologyID, AxiomType, OWLOntology
-from org.semanticweb.owlapi.model import AddOntologyAnnotation
-from org.semanticweb.owlapi.model import OWLRuntimeException
-from org.semanticweb.owlapi.formats import RDFXMLDocumentFormat
-from uk.ac.manchester.cs.owlapi.modularity import SyntacticLocalityModuleExtractor
-from uk.ac.manchester.cs.owlapi.modularity import ModuleType
-from com.google.common.base import Optional
-from org.semanticweb.owlapi.io import OWLOntologyCreationIOException
-from org.semanticweb.owlapi.model import OWLOntologyFactoryNotFoundException
+from org.semanticweb.owlapi.model import IRI
 from org.semanticweb.owlapi.model.parameters import Imports as ImportsEnum
-from org.semanticweb.owlapi.reasoner import InferenceType
 from org.semanticweb.owlapi.util import InferredSubClassAxiomGenerator
 from org.semanticweb.owlapi.util import InferredEquivalentClassAxiomGenerator
 from org.semanticweb.owlapi.util import InferredSubDataPropertyAxiomGenerator
