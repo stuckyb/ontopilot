0c761255c1d4c76037dd8e0188f8ae8468c42f02
stuckyb <stuckyb@flmnh.ufl.edu>
2017-03-17 13:49:07 -0400

    Updated license/copyright information in testing code.

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env jython
 
-# Copyright (C) 2016 Brian J. Stucky
+# Copyright (C) 2017 Brian J. Stucky
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
