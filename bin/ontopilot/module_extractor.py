1d8d9084a4c9fdf352f3c52810d77ec61efc26eb
stuckyb <stuckyb@flmnh.ufl.edu>
2017-03-23 12:16:30 -0400

    First steps toward implementing custom module extraction methods.  Single-term extraction is now mostly complete.

---
+++ bin/ontopilot/module_extractor.py
@@ -0,0 +1,206 @@
+# Copyright (C) 2017 Brian J. Stucky
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Python imports.
+from idresolver import IDResolver
+from ontology import Ontology
+from ontology_entities import _OntologyClass, _OntologyDataProperty
+from ontology_entities import _OntologyObjectProperty, _OntologyAnnotationProperty
+from ontology_entities import _OntologyIndividual, _OntologyEntity
+from reasoner_manager import ReasonerManager
+from observable import Observable
+
+# Java imports.
+from java.io import File, FileOutputStream
+from java.util import HashSet
+from org.semanticweb.owlapi.apibinding import OWLManager
+from org.semanticweb.owlapi.model import IRI, OWLOntologyID
+from org.semanticweb.owlapi.model import AddAxiom, AddImport, RemoveImport
+from org.semanticweb.owlapi.model import SetOntologyID, AxiomType, OWLOntology
+from org.semanticweb.owlapi.model import AddOntologyAnnotation
+from org.semanticweb.owlapi.formats import RDFXMLDocumentFormat
+from uk.ac.manchester.cs.owlapi.modularity import SyntacticLocalityModuleExtractor
+from uk.ac.manchester.cs.owlapi.modularity import ModuleType
+from com.google.common.base import Optional
+from org.semanticweb.owlapi.io import OWLOntologyCreationIOException
+from org.semanticweb.owlapi.model import OWLOntologyFactoryNotFoundException
+from org.semanticweb.owlapi.model.parameters import Imports as ImportsEnum
+
+
+# Define constants for the supported extraction methods.
+class _ExtractMethodsStruct:
+    # The STAR syntactic locality extraction method.
+    LOCALITY = 0
+    # Extract single terms without any other axioms (except annotations).
+    SINGLE = 1
+    # Extract terms and their superclass/superproperty hierarchies without any
+    # other axioms (except annotations).
+    HIERARCHY = 2
+
+    # Combine all supported methods in a single tuple.
+    all_methods = (LOCALITY, SINGLE, HIERARCHY)
+
+    # Define string values that map to the extraction methods.
+    strings = {
+        'locality': LOCALITY,
+        'single': SINGLE,
+        'hierarchy': HIERARCHY
+    }
+methods = _ExtractMethodsStruct()
+
+
+class ModuleExtractor:
+    """
+    Extracts import "modules" from existing OWL ontologies.
+    """
+    # The IRI for the "dc:source" annotation property.
+    SOURCE_PROP_IRI = IRI.create('http://purl.org/dc/elements/1.1/source')
+
+    # The IRI for inferred axiom annotation.
+    INFERRED_ANNOT_IRI = IRI.create(
+        'http://www.geneontology.org/formats/oboInOwl#is_inferred'
+    )
+
+    def __init__(self, ontology_source):
+        """
+        Initialize this ModuleExtractor instance.  The argument
+        "ontology_source" should be an instance of ontopilot.Ontology.
+        """
+        self.ontology = ontology_source
+        self.owlont = self.ontology.getOWLOntology()
+
+        # Initialize data structures for holding the extraction signatures.
+        self.signatures = {}
+        for method in methods.all_methods:
+            self.signatures[method] = []
+
+    def addEntity(self, entity_id, method):
+        """
+        Adds an entity to the module signature.
+
+        entity_id: The identifier of the entity.  Can be either an OWL API IRI
+            object or a string containing: a label (with or without a prefix),
+            a prefix IRI (i.e., a curie, such as "owl:Thing"), a relative IRI,
+            a full IRI, or an OBO ID (e.g., a string of the form "PO:0000003").
+            Labels should be enclosed in single quotes (e.g., 'label text' or
+            prefix:'label txt').
+        method: The extraction method to use for this entity.
+        """
+        entity = self.ontology.getExistingEntity(entity_id)
+        if entity == None:
+            raise RuntimeError(
+                'The entity {0} could not be found in the source '
+                'ontology.'.format(entity_id)
+            )
+
+        self.signatures[method].append(entity)
+
+    def extractModule(self, mod_iri):
+        """
+        Extracts a module that is a subset of the entities in the source
+        ontology.  The result is returned as an Ontology object.
+
+        mod_iri: The IRI for the extracted ontology module.  Can be either an
+            IRI object or a string containing a relative IRI, prefix IRI, or
+            full IRI.
+        """
+        target = Ontology(self.ontology.ontman.createOntology())
+
+        self._extractSingleTerms(self.signatures[methods.SINGLE], target)
+
+        return target
+
+    def _extractSingleTerms(self, signature, target):
+        """
+        Extracts terms from the source ontology using the "single term"
+        extraction method, which means pulling individual terms without any
+        associated axioms (except for annotations).  Annotation properties that
+        are used to annotate entities in the signature will also be extracted
+        from the source ontology.
+
+        signature: A list of OntologyEntity objects.
+        target: The target module ontopilot.Ontology object.
+        """
+        owltarget = target.getOWLOntology()
+
+        rdfslabel = self.ontology.df.getRDFSLabel()
+
+        while len(signature) > 0:
+            entity = signature.pop()
+
+            owlent = entity.getOWLAPIObj()
+
+            # Get the declaration axiom for this entity and add it to the
+            # target ontology.
+            ontset = self.owlont.getImportsClosure()
+            for ont in ontset:
+                dec_axioms = ont.getDeclarationAxioms(owlent)
+                self.ontology.ontman.addAxioms(owltarget, dec_axioms)
+
+            # Get all annotation axioms for this entity and add them to the
+            # target ontology.
+            for ont in ontset:
+                annot_axioms = ont.getAnnotationAssertionAxioms(owlent.getIRI())
+                self.ontology.ontman.addAxioms(owltarget, annot_axioms)
+
+                # Check if the relevant annotation properties are already
+                # included in the target ontology.  If not, add them to the set
+                # of terms to extract.
+                for annot_axiom in annot_axioms:
+                    # Ignore rdfs:label.
+                    if annot_axiom.getProperty().equals(rdfslabel):
+                        continue
+
+                    prop_iri = annot_axiom.getProperty().getIRI()
+
+                    if target.getExistingAnnotationProperty(prop_iri) == None:
+                        annot_ent = self.ontology.getExistingAnnotationProperty(prop_iri)
+                        # Built-in annotation properties, such as rdfs:label,
+                        # will not "exist" because they have no declaration
+                        # axioms, so we need to check for this.
+                        if annot_ent != None:
+                            signature.append(annot_ent)
+
+    def _extractModule(self, signature, mod_iri):
+        """
+        Extracts a module that is a subset of the entities in this ontology.
+        The result is returned as an Ontology object.
+
+        signature: A Java Set of all entities to include in the module.
+        mod_iri: The IRI for the extracted ontology module.  Can be either an
+            IRI object or a string containing a relative IRI, prefix IRI, or
+            full IRI.
+        """
+        modIRI = self.idr.expandIRI(mod_iri)
+
+        slme = SyntacticLocalityModuleExtractor(
+            self.ontman, self.getOWLOntology(), ModuleType.STAR
+        )
+        modont = Ontology(slme.extractAsOntology(signature, modIRI))
+
+        # Add an annotation for the source of the module.
+        sourceIRI = None
+        ontid = self.getOWLOntology().getOntologyID()
+        if ontid.getVersionIRI().isPresent():
+            sourceIRI = ontid.getVersionIRI().get()
+        elif ontid.getOntologyIRI().isPresent():
+            sourceIRI = ontid.getOntologyIRI().get()
+
+        if sourceIRI != None:
+            modont.setOntologySource(sourceIRI)
+
+        return modont
+
