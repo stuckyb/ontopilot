80c75f24e89bdd1ae4edf3f5fc65ea6b82134503
stuckyb <stuckyb@flmnh.ufl.edu>
2017-04-19 14:38:56 -0400

    Rewrote parts of Main.java to make the console/interpreter instantiation and handling compliant with the architecture of Jython 2.7.

--- java-src/Main.java
+++ java-src/Main.java
@@ -2,60 +2,63 @@ import java.io.FileInputStream;
 import java.lang.System;
 import java.util.Properties;
 import java.util.Arrays;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import org.python.core.Py;
 import org.python.core.PyException;
 import org.python.core.PyFile;
 import org.python.core.PySystemState;
-import org.python.util.JLineConsole;
-import org.python.util.InteractiveConsole;
-import org.python.util.InteractiveInterpreter;
+import org.python.util.PythonInterpreter;
+import org.python.core.PlainConsole;
+
 
 public class Main {
-    private static InteractiveConsole newInterpreter(boolean interactiveStdin) {
-        if (!interactiveStdin) {
-            return new InteractiveConsole();
-        }
-
-        String interpClass = PySystemState.registry.getProperty(
-            "python.console", ""
-        );
-        if (interpClass.length() > 0) {
-            try {
-                return (InteractiveConsole)Class.forName(
-            interpClass).newInstance();
-            } catch (Throwable t) {
-                // fall through
-            }
-        }
-
-        return new InteractiveConsole();
-    }
-
     public static void main(String[] args) throws PyException, Exception {
-        // To make the python interpreter accept arguments at right index.
+        Logger logger = Logger.getLogger("Main");
+
+        // Use PlainConsole for all console I/O.  OntoPilot doesn't need the
+        // more complicated features of JLineConsole.
+        System.getProperties().setProperty(
+            "python.console", "org.python.core.PlainConsole"
+        );
+
+        // To make the python interpreter accept arguments at the right index.
         String[] args_new = new String[args.length + 1];
         args_new[0] = "";
         for(int i = 1; i < args_new.length; i++) {
             args_new[i] = args[i - 1];
         }
-        args = args_new;
-	
+
         PySystemState.initialize(
-            PySystemState.getBaseProperties(), new Properties(), args
+            PySystemState.getBaseProperties(), new Properties(), args_new
         );
 
         PySystemState systemState = Py.getSystemState();
 
-        // Decide if stdin is interactive.
+        // Decide if stdin is interactive; if not, set the primary and
+        // secondary prompts (sys.ps1 and sys.ps2) to the empty string.
         boolean interactive = ((PyFile)Py.defaultSystemState.stdin).isatty();
         if (!interactive) {
             systemState.ps1 = systemState.ps2 = Py.EmptyString;
         }
-         
+
         // Create an interpreter.
-        InteractiveConsole interp = newInterpreter(interactive);
+        PythonInterpreter interp = new PythonInterpreter();
         systemState.__setattr__("_jy_interpreter", Py.java2py(interp));
+
+        // Verify that we have the correct console type.  Don't use
+        // "instanceof" here because that will return true for parent/child
+        // class comparisons.
+	if (Py.getConsole().getClass() != PlainConsole.class) {
+            logger.log(
+                Level.WARNING, "Expected the interpreter console to be of " +
+                "type org.python.core.PlainConsole, but instead found " +
+                Py.getConsole().getClass().getName() + "."
+            );
+        }
+
+	// Run OntoPilot.
         interp.exec("try:\n import ontopilot_main\nexcept SystemExit: pass");
     }
 }
