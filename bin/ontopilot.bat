57b2930d9c38e2c8abdc70d616ceced17ee8df7f
stuckyb <stuckyb@flmnh.ufl.edu>
2017-04-21 00:47:30 -0400

    Fixed a few bugs and made some relatively minor enhancements to the Windows launch script.  In particular, got it working with Windows 7.

--- bin/ontopilot.bat
+++ bin/ontopilot.bat
@@ -1,6 +1,6 @@
 @echo off
 
-setlocal 
+setlocal enabledelayedexpansion
 
 rem The location of the main OntoPilot program, relative to this launch script.
 set ONTOPILOTPATH=..\python-src\ontopilot_main.py
@@ -17,34 +17,28 @@ rem symlink, the next two lines are all we need.
 set SRCPATH=%~f0
 set SRCDIR=%~dp0
 
-rem If SRCPATH is a symlink, resolve the link (and any subsequent links) until we
-rem arrive at the actual script location.
+rem If SRCPATH is a symlink, resolve the link (and any subsequent links) until
+rem we arrive at the actual script location.
 :while
 dir "%SRCPATH%" | find "<SYMLINK>" >nul && (
- 	for /f "tokens=2 delims=[]" %%i in ('dir "%SRCPATH%"* ^| find "<SYMLINK>"') do set link=%%i
+ 	for /f "tokens=2 delims=[]" %%i in ('dir "!SRCPATH!" ^| find "<SYMLINK>"') do set SRCPATH=%%i
 
- 	set SRCPATH=%link%
-
- 	rem If the link target is a relative path, it is relative to the original
-	rem symlink location, so we must construct a new path for the link target
-	rem based on SRCDIR (the original symlink location).
- 	if "%SRCPATH:~1,1%" neq ":" (
- 		set SRCPATH=%SRCDIR%\%SRCPATH%
+	rem If the link target is a relative path, it is relative to the
+	rem original symlink location, so we must construct a new path for the
+	rem link target based on SRCDIR (the original symlink location).
+ 	if "!SRCPATH:~1,1!" neq ":" (
+ 		set SRCPATH=%SRCDIR%!SRCPATH!
  	)
 
- 	for %%m in ("%link%") do (
+ 	for %%m in ("!SRCPATH!") do (
  		set SRCDIR=%%~dpm
  	)
 
- 	dir "%SRCPATH%" | find "<SYMLINK>" >nul && (
- 		goto :while
- 	)
+ 	goto :while
 )
 
-endlocal
-
 rem Check if java is installed.
-where java >nul 2>&1
+where %JAVAPATH% >nul 2>&1
 if %ERRORLEVEL% neq 0 (
 	echo ERROR: The Java run-time environment appears to be missing.
 	echo Please install Java in order to run OntoPilot.
@@ -53,3 +47,5 @@ if %ERRORLEVEL% neq 0 (
 
 rem Run OntoPilot, passing on all command-line arguments.
 %JAVAPATH% -jar "%SRCDIR%%JYTHONPATH%" "%SRCDIR%%ONTOPILOTPATH%" %*
+
+endlocal
