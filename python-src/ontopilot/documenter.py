4bc043f35ae7f96b7fb28225427cb2d2de4eb4cd
stuckyb <stuckyb@flmnh.ufl.edu>
2017-06-09 21:44:43 -0400

    Start of auto documentation generator module.  Initial implementation of parsing documentation specification and building an internal data structure that is an abstract representation of the documentation.

---
+++ python-src/ontopilot/documenter.py
@@ -0,0 +1,267 @@
+# Copyright (C) 2017 Brian J. Stucky
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#
+# Generates documentation for the entities in a source ontology.
+#
+
+# Python imports.
+from __future__ import unicode_literals
+from module_extractor import ModuleExtractor, rel_axiom_types
+from obohelper import termIRIToOboID, OBOIdentifierError
+from ontopilot import logger
+from ontopilot import TRUE_STRS
+import yaml
+
+# Java imports.
+
+
+class DocumentSpecificationError(RuntimeError):
+    """
+    Exception for errors detected in the documentation specification.
+    """
+    pass
+
+
+class _DocumentSection:
+    """
+    An internal "struct" that represents one top-level section in an ontology
+    documentation document.
+    """
+    def __init__(self):
+        self.title = ''
+        self.docnodes = []
+
+    def __str__(self):
+        docnodes_str = ''
+        for docnode in self.docnodes:
+            docnodes_str += '\n' + str(docnode)
+
+        # Handle the case where docnodes is empty.
+        if docnodes_str == '':
+            docnodes_str = '\n'
+
+        return (
+            'Title: {0}\nEntities:{1}'.format(
+                self.title, docnodes_str
+            )
+        )
+
+
+class _DocumentNode:
+    """
+    An internal class for building a tree-like data structure that
+    represents ontology entities in an ontology documentation document.
+    """
+    def __init__(self, entID, srcont):
+        """
+        Initializes this _DocumentNode with information from the ontology
+        entity specified by entID.
+
+        entID: An entity identifier.
+        srcont: The source ontopilot.Ontology object.
+        """
+        entity = srcont.getExistingEntity(entID)
+        if entity is None:
+            raise DocumentSpecificationError(
+                'No entity with the ID "{0}" could be found in the source ontology.  Please provide the correct entity ID in the documentation specification.'.format(entID)
+            )
+
+        self.entIRI = entity.getIRI().toString()
+        try:
+            self.entOBO_ID = termIRIToOboID(self.entIRI)
+        except OBOIdentifierError:
+            self.entOBO_ID = ''
+
+        labels = entity.getLabels()
+        if len(labels) > 0:
+            self.entlabel = labels[0]
+        else:
+            self.entlabel = ''
+
+        defs = entity.getDefinitions()
+        if len(defs) > 0:
+            self.entdef = defs[0]
+        else:
+            self.entdef = ''
+
+        self.children = []
+
+        self.entity = entity
+        self.ont = srcont
+
+    def getDescendants(self, maxdepth, curdepth=1, entset=None):
+        """
+        Recursively retrieves all descendents of the ontology entity contained
+        by this DocumentNode up to the specified maxdepth.  If maxdepth == -1,
+        all descendents are retrieved.  This method is also safe for ontologies
+        that include cycles in their descendant relationships.
+        """
+        if entset is None:
+            # Initialize a set to track all entities that are seen during the
+            # recursion.  This is necessary to avoid getting stuck in circular
+            # descendant relationships.
+            entset = set(self.entIRI)
+
+        me = ModuleExtractor(self.ont)
+
+        # Get the direct descendants of this node's entity.
+        components = me.getDirectlyRelatedComponents(
+            self.entity.getOWLAPIObj(), (rel_axiom_types.DESCENDANTS,)
+        )
+        children = components[0]
+
+        for child in children:
+            iristr = child.getIRI().toString()
+            if iristr not in entset:
+                entset.add(iristr)
+                childnode = _DocumentNode(iristr, self.ont)
+                self.children.append(childnode)
+                if curdepth < maxdepth or maxdepth == -1:
+                    childnode.getDescendants(maxdepth, curdepth + 1, entset)
+
+    def _toIndentedStr(self, indentlevel):
+        """
+        Generates and returns a string representation of this _DocumentNode,
+        indented according to the specified indentlevel.
+        """
+        indent = '    ' * indentlevel
+        strs = [
+            'IRI: ' + self.entIRI,
+            'OBO ID: ' + self.entOBO_ID,
+            'Label: ' + self.entlabel
+        ]
+        if len(self.children) > 0:
+            strs.append('Children:')
+
+        retstr = '\n'.join([indent + strval for strval in strs])
+        retstr += '\n'
+
+        childcnt = 0
+        for child in self.children:
+            childcnt += 1
+            retstr += child._toIndentedStr(indentlevel + 1)
+            if childcnt < len(self.children):
+                retstr += '\n'
+
+        return retstr
+
+    def __str__(self):
+        return self._toIndentedStr(1)
+
+
+class Documenter:
+    def __init__(self, src_ont):
+        """
+        Initializes this Documenter with a source ontology.
+
+        src_ont: An ontopilot.Ontology instance.
+        """
+        self.ont = src_ont
+
+    def _buildDocumentNode(self, rawdocnode):
+        """
+        Builds a _DocumentNode objects that corresponds with the raw data
+        structure parsed from a strict YAML documentation specification.
+        """
+        if 'ID' not in rawdocnode:
+            raise DocumentSpecificationError(
+                'No entity ID was provided for an element in the documentation specification.  An ID must be provided for each ontology entity you wish to include in the ontology documentation.  The element with the missing ID is: {0}.'.format(rawdocnode)
+            )
+
+        docnode = _DocumentNode(rawdocnode['ID'], self.ont)
+
+        if 'children' in rawdocnode:
+            for child in rawdocnode['children']:
+                childnode = self._buildDocumentNode(child)
+                docnode.children.append(childnode)
+        elif 'descendants' in rawdocnode:
+            # Get the value of maxdepth for the call to getDescendants() that
+            # corresponds with the value of the "descendants" directive.
+            if rawdocnode['descendants'].lower() == 'all':
+                maxdepth = -1
+            else:
+                try:
+                    maxdepth = int(rawdocnode['descendants'])
+                except ValueError:
+                    raise DocumentSpecificationError(
+                        'Invalid value for "descendants" directive in document specification file: "{0}".'.format(rawdocnode['descendants'])
+                    )
+
+            docnode.getDescendants(maxdepth)
+
+        return docnode
+
+    def _buildDocumentSection(self, rawsection):
+        """
+        Builds a _DocumentSection object that corresponds with the raw section
+        data structure parsed from a strict YAML documentation specification.
+        """
+        title_strs = rawsection.keys()
+        if len(title_strs) != 1:
+            raise DocumentSpecificationError(
+                'Each documentation section must have exactly one heading.  The current documentation section has the following headings: {0}.  Please correct the documentation specification file.'.format('"' + '", "'.join(title_strs) + '"')
+            )
+
+        title = title_strs[0].strip()
+
+        new_section = _DocumentSection()
+        new_section.title = title
+
+        for rawdocnode in rawsection[title_strs[0]]:
+            docnode = self._buildDocumentNode(rawdocnode)
+            new_section.docnodes.append(docnode)
+
+        return new_section
+
+    def _buildDocumentDS(self, docspec):
+        """
+        Builds a data structure that captures the components of the ontology
+        and relevant information from the docspecs that will be used to
+        generate the final documentation.  The data structure is a list of
+        _DocumentSection objects.
+
+        specs: Documentation specification parsed from a YAML source.
+        """
+        document = []
+
+        for rawsection in docspec:
+            section = self._buildDocumentSection(rawsection)
+            document.append(section)
+        
+        return document
+
+    def _writeMarkdown(self, doctree, fileout):
+        """
+        Uses a _DocumentNode tree to generate an output Markdown document.
+        """
+
+    def document(self, docspec, fileout):
+        """
+        Generates Markdown documentation for the source ontology according to a
+        documentation specification provided in YAML format.
+
+        docspec: A source of YAML-formatted documentation specification
+            information.  Can be either a byte string, regular (or unicode)
+            string, or a file object.
+        fileout: A writable file object.
+        """
+        parsed_docspec = yaml.load(docspec)
+        print parsed_docspec
+
+        docDS = self._buildDocumentDS(parsed_docspec)
+        for section in docDS:
+            print section
+
